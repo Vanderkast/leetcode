9bf11c8482069c049e3f49efac0924ec5fc112ea
Valentine Mazurov <valentine.mazurov@yandex.ru>
2021-10-25 18:00:52 +0300

    init

---
+++ 222/src/main/java/com/vanderkast/leetcode/CountCompleteTreeNodes.java
@@ -0,0 +1,12 @@
+package com.vanderkast.leetcode;
+
+import net.vanderkast.leetcode.commons.TreeNode;
+
+import java.util.Stack;
+
+public class CountCompleteTreeNodes {
+    public int countNodes(TreeNode cursor) {
+        // todo implement me
+        throw new UnsupportedOperationException();
+    }
+}
