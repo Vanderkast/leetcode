1acea019dde95a921f224a48d34bf0a140528aff
Valentine Mazurov <vmazurov@dalet.com>
2021-12-07 09:50:53 +0300

    r328 init

---
+++ r328/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+
+struct Solution;
+
+impl Solution {
+    pub fn odd_even_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Option::None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
