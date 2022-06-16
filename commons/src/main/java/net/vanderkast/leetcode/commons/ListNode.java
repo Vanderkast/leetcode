73eed32d9c5ae3eefbe031761a4ef70c9d8d046b
Valentine Mazurov <vmazurov@dalet.com>
2022-06-16 16:02:17 +0300

    j21 solution recovered from leetcode

--- commons/src/main/java/net/vanderkast/leetcode/commons/ListNode.java
+++ commons/src/main/java/net/vanderkast/leetcode/commons/ListNode.java
@@ -1,20 +1,19 @@
 package net.vanderkast.leetcode.commons;
 
-import java.util.List;
 import java.util.Objects;
 
 public class ListNode {
     public int val;
     public ListNode next;
 
-    ListNode() {
+    public ListNode() {
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
