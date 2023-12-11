429ffba4b48996201859598b9205e8bb8cfaa0e6
Valentine Mazurov <valentine.mazurov@yandex.ru>
2023-12-11 10:00:08 +0300

    j1287. Element Appearing More Than 25% In Sorted Array

---
+++ java-solutions/src/main/java/com/vanderkast/leetcode/j1287/j1287/ElementAppearingMoreThan25InSortedArray.java
@@ -0,0 +1,19 @@
+package com.vanderkast.leetcode.j1287.j1287;
+
+public class ElementAppearingMoreThan25InSortedArray {
+    public int findSpecialInteger(int[] arr) {
+        int threshold = arr.length / 4;
+        int prev = arr[0];
+        int count = 1;
+        for (int i = 1; i < arr.length; ++i) {
+            if (prev == arr[i])
+                ++count;
+            else
+                count = 1;
+            if (count > threshold)
+                return arr[i];
+            prev = arr[i];
+        }
+        return prev;
+    }
+}
