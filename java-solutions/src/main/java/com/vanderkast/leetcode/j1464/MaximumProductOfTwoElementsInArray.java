4ec72f08da236c5568a04737d0a29629063c35d1
Valentine Mazurov <valentine.mazurov@yandex.ru>
2023-12-12 07:09:32 +0300

    j1464. Maximum Product of Two Elements in an Array

---
+++ java-solutions/src/main/java/com/vanderkast/leetcode/j1464/MaximumProductOfTwoElementsInArray.java
@@ -0,0 +1,16 @@
+package com.vanderkast.leetcode.j1464;
+
+public class MaximumProductOfTwoElementsInArray {
+    public int maxProduct(int[] nums) {
+        int m1 = 0, m2 = 0;
+        for (int x : nums) {
+            if (x > m1) {
+                m2 = m1;
+                m1 = x;
+            } else if (x > m2) {
+                m2 = x;
+            }
+        }
+        return (m1 - 1) * (m2 - 1);
+    }
+}
