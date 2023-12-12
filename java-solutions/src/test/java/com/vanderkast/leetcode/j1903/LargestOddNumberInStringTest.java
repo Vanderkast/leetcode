4c5df92dc104bc69a85c72eb910c6e3d9878bda7
Valentine Mazurov <valentine.mazurov@yandex.ru>
2023-12-12 07:36:41 +0300

    j1903. Largest Odd Number in String

---
+++ java-solutions/src/test/java/com/vanderkast/leetcode/j1903/LargestOddNumberInStringTest.java
@@ -0,0 +1,29 @@
+package com.vanderkast.leetcode.j1903;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LargestOddNumberInStringTest {
+    private final LargestOddNumberInString solution = new LargestOddNumberInString();
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    void test(String num, String expected) {
+        var actual = solution.largestOddNumber(num);
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> parameters() {
+        return Stream.of(
+                Arguments.arguments("1", "1"),
+                Arguments.arguments("52", "5"),
+                Arguments.arguments("4206", ""),
+                Arguments.arguments("35427", "35427")
+        );
+    }
+}
